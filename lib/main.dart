dc3e4086c8d035fb95fdfd48b50e0c2d73b69cb1
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-01-14 23:12:25 +0100

    Entry widget class

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,93 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+extension TruncateStringExtension on String {
+  String truncateTo(int maxLength) =>
+      (this.length <= maxLength) ? this : '${this.substring(0, maxLength)}...';
+}
 
-class MyApp extends StatelessWidget {
+void main() {
+  runApp(
+    MicroblogApp(),
+  );
+}
+
+class MicroblogApp extends StatelessWidget {
+  const MicroblogApp({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: const Text('Hello World'),
-        ),
+      title: 'Devlog Microblog Client',
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final List<MicroblogEntry> _entries = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: true,
+      appBar: AppBar(
+        title: Text('Devlog Microblog Client'),
+      ),
+    );
+  }
+}
+
+class MicroblogEntry extends StatelessWidget {
+  MicroblogEntry({this.date, this.title, this.text});
+  final String title;
+  final String text;
+  final DateTime date;
+
+  @override
+  Widget build(BuildContext context) {
+    final dateFormatted =
+        '${date.day.toString().padLeft(2, "0")}.${date.month.toString().padLeft(2, "0")}';
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(right: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  dateFormatted,
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                Text(
+                  date.year.toString(),
+                  style: Theme.of(context).textTheme.headline6,
+                )
+              ],
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 5),
+                child: Text(text.truncateTo(30)),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
