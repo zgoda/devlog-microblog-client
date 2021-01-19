1ad1c5a94c849a8673e43d9dca3dfe3c088fe294
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-01-19 19:50:47 +0100

    How do I load initial state?

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsScreen extends StatefulWidget {
   @override
@@ -7,8 +8,21 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   final _formKey = GlobalKey<FormState>();
-  bool unsecuredTransport = false;
-  bool saveCredentials = true;
+  bool _unsecuredTransport;
+  bool _saveCredentials;
+  String _host;
+
+  @override
+  void initState() {
+    SharedPreferences.getInstance().then((prefs) {
+      setState(() {
+        _host = prefs.getString('host');
+        _unsecuredTransport = prefs.getBool('unsecuredTransport');
+        _saveCredentials = prefs.getBool('storeCredentials');
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,41 +37,55 @@ class _SettingsScreenState extends State<SettingsScreen> {
           children: <Widget>[
             TextFormField(
               decoration: InputDecoration(
+                labelText: 'Host',
                 hintText: 'Host name or IP address',
                 contentPadding: EdgeInsets.all(10),
               ),
+              initialValue: _host,
+              onChanged: (value) {
+                setState(() {
+                  _host = value;
+                });
+              },
             ),
             SwitchListTile(
               title: const Text('Use unsecured transport'),
-              value: unsecuredTransport,
+              value: _unsecuredTransport,
               onChanged: (value) {
                 setState(() {
-                  unsecuredTransport = value;
+                  _unsecuredTransport = value;
                 });
               },
             ),
             SwitchListTile(
               title: const Text('Store login credentials'),
-              value: saveCredentials,
+              value: _saveCredentials,
               onChanged: (value) {
                 setState(() {
-                  saveCredentials = value;
+                  _saveCredentials = value;
                 });
               },
             ),
             ElevatedButton(
               child: Text('Save'),
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('Form data saved'),
-                  ),
-                );
-              },
+              onPressed: _saveSettings,
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _saveSettings() {
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.setBool('unsecuredTransport', _unsecuredTransport);
+      prefs.setBool('storeCredentials', _saveCredentials);
+      prefs.setString('host', _host);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Form data saved'),
+        ),
+      );
+    });
   }
 }
