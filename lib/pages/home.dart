85419a42050f71526940aa09531170cf88f11d29
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-01-31 20:36:12 +0100

    This is normal widget, does not use any hooks

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class HomeScreen extends HookWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Future<void> future = locator.allReady();
