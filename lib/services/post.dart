00e504392fef6da62e18d1bac22251f77e04da2e
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-02-14 23:38:57 +0100

    Service to update post data

--- lib/services/post.dart
+++ lib/services/post.dart
@@ -20,6 +20,15 @@ class PostListNotifier extends StateNotifier<List<Post>> {
   void addAll(List<Post> posts) {
     state = [...posts, ...state];
   }
+
+  void update(Post post) {
+    final newState = [...state];
+    final postIndex = newState.indexWhere((item) => item.pk == post.pk);
+    if (postIndex > -1) {
+      newState[postIndex] = post;
+      state = newState;
+    }
+  }
 }
 
 final postCollectionServiceProvider = Provider<PostService>((ref) {
@@ -34,65 +43,76 @@ class PostService {
   Uri _collectionUrl;
   var _currentPage = 1;
   final _http = http.Client();
+  bool _useHttps;
+  String _host;
 
-  static const ENDPOINT = 'quips';
+  static const COLLECTION_ENDPOINT = 'quips';
+  static const ITEM_ENDPOINT = 'quip';
 
   AuthenticationService _auth;
   PostListNotifier _notifier;
 
   PostService(UserSettingsModel prefs, AuthenticationService auth) {
+    _host = prefs.host;
+    _useHttps = !prefs.unsecuredTransport;
     _collectionUrl = buildServerUrl(
-      prefs.host,
-      buildEndpointPath(ENDPOINT),
-      secure: !prefs.unsecuredTransport,
+      _host,
+      buildEndpointPath(COLLECTION_ENDPOINT),
+      secure: _useHttps,
     );
     _auth = auth;
   }
 
   set notifier(PostListNotifier pln) => _notifier = pln;
 
+  Uri _itemUrl(Post post) {
+    final path =
+        [buildEndpointPath(ITEM_ENDPOINT), post.pk.toString()].join('/');
+    return buildServerUrl(_host, path, secure: _useHttps);
+  }
+
   Future<int> fetchCollection({int page: 1}) async {
     final List<Post> posts = [];
     final url = _collectionUrl.replace(queryParameters: {'p': page.toString()});
-    if (!_auth.hasToken()) {
-      await _auth.login();
-    }
-    Map<String, String> headers = _auth.authHeader();
-    http.Response resp = await _http.get(url, headers: headers);
-    if (resp.statusCode == 400) {
-      await _auth.login();
-      headers = _auth.authHeader();
-      resp = await http.get(url, headers: headers);
-    }
+    await _auth.login();
+    final headers = _auth.authHeader();
+    final resp = await _http.get(url, headers: headers);
     if (resp.statusCode == 200) {
-      final Map<String, dynamic> respData = jsonDecode(resp.body);
+      final respData = jsonDecode(resp.body);
       posts.addAll(
         respData['quips'].map<Post>((item) => Post.fromJson(item)).toList(),
       );
       _currentPage = page;
     }
-    _notifier.addAll(posts);
+    if (posts != []) {
+      _notifier.addAll(posts);
+    }
     return _currentPage;
   }
 
   Future<void> addPost(Post post) async {
-    if (!_auth.hasToken()) {
-      await _auth.login();
-    }
-    var headers = _auth.authHeader();
-    headers['Content-Type'] = 'application/json';
     final requestBody = jsonEncode(post.toJson());
-    var resp =
+    await _auth.login();
+    final headers = _auth.authHeader();
+    headers['Content-Type'] = 'application/json';
+    final resp =
         await _http.post(_collectionUrl, headers: headers, body: requestBody);
-    if (resp.statusCode == 400) {
-      await _auth.login();
-      headers.addAll(_auth.authHeader());
-      resp =
-          await _http.post(_collectionUrl, headers: headers, body: requestBody);
-    }
     if (resp.statusCode < 300) {
-      final newPost = Post.fromJson(jsonDecode(resp.body));
+      final newPost = Post.fromJson(jsonDecode(resp.body)['quip']);
       _notifier.add(newPost);
+    }
+  }
+
+  Future<void> updatePost(Post post) async {
+    final url = _itemUrl(post);
+    final requestBody = jsonEncode(post.toJson());
+    await _auth.login();
+    final headers = _auth.authHeader();
+    headers['Content-Type'] = 'application/json';
+    final resp = await _http.put(url, headers: headers, body: requestBody);
+    if (resp.statusCode < 300) {
+      final newPost = Post.fromJson(jsonDecode(resp.body)['quip']);
+      _notifier.update(newPost);
     }
   }
 }
